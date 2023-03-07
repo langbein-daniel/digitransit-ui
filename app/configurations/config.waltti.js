03144dfdd25d303089a00c7029d9f79becbd2234
Daniel Langbein <daniel@systemli.org>
2023-03-07 13:26:15 +0100

    docs: add documentation for showNearYouButtons, nearbyModeSet and parkAndRide.showParkAndRide

--- app/configurations/config.waltti.js
+++ app/configurations/config.waltti.js
@@ -135,6 +135,12 @@ export default {
     },
   },
 
+  // Different icons.
+  //
+  // CtrlPanel.NearStopsAndRoutes
+  //   Show button links to near you page for different travel modes
+  //   modeSet={config.nearbyModeSet || config.iconModeSet}
+  //   function getIconName(mode, modeSet)
   nearbyModeSet: 'waltti',
 
   redirectReittiopasParams: true,
@@ -181,6 +187,7 @@ export default {
   parkingAreaSources: ['liipi'],
 
   parkAndRide: {
+    // MapLayersDialogContent -> User can enable/disable "Park & ride sites" map layer
     showParkAndRide: false,
     parkAndRideMinZoom: 13,
     pageContent: {
